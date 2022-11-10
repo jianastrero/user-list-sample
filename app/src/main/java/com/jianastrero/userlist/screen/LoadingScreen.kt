83e64144754bc85d0b55e4e92bc151747aa2e235
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-11 02:43:02 +0800

    create LoadingScreen

---
+++ app/src/main/java/com/jianastrero/userlist/screen/LoadingScreen.kt
@@ -0,0 +1,31 @@
+package com.jianastrero.userlist.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by jianj on 11/11/2022.
+ */
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview
+@Composable
+private fun LoadingScreenPreview() {
+    LoadingScreen()
+}
