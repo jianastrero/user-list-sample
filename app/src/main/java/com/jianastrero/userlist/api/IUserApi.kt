ced2c869303f317a39b1df260154b1b9b7d7df22
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-10 22:49:43 +0800

    create IUserApi.kt

---
+++ app/src/main/java/com/jianastrero/userlist/api/IUserApi.kt
@@ -0,0 +1,15 @@
+package com.jianastrero.userlist.api
+
+import com.jianastrero.userlist.model.UserModel
+import retrofit2.http.GET
+
+/**
+ * Created by jianj on 11/10/2022.
+ */
+
+interface IUserApi {
+
+    @GET("erni-ph-mobile-team/c5b401c4fad718da9038669250baff06/raw/7e390e8aa3f7da4c35b65b493fcbfea3da55eac9/test.json")
+    fun getUsers(): List<UserModel>
+
+}
