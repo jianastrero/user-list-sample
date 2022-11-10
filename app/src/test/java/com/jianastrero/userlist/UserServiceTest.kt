d005c18ccd73cb50ac74bc9b70499d4ced5eba58
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-11 00:28:27 +0800

    create unit tests for User Service

---
+++ app/src/test/java/com/jianastrero/userlist/UserServiceTest.kt
@@ -0,0 +1,38 @@
+package com.jianastrero.userlist
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.jianastrero.userlist.model.UserModel
+import com.jianastrero.userlist.service.IUserService
+import com.jianastrero.userlist.service.implementation.UserService
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+/**
+ * Created by jianj on 11/11/2022.
+ */
+
+class UserServiceTest {
+
+    private val userApi = Retrofit.userApi()
+    private val gson = Gson()
+    private val userService: IUserService  = UserService(userApi)
+
+    @Test
+    fun `Test Get User`() = runBlocking {
+        val actual = userService.getUsers()
+            .sortedBy { it.id }
+
+        val expectedJson = readFileFromResources("test-distinct.json")
+        val expected = gson
+            .fromJson<List<UserModel>>(
+                expectedJson,
+                object : TypeToken<List<UserModel>>() {}.type
+            )
+            .sortedBy { it.id }
+            .reversed()
+
+        assert(expected.containsAll(actual) && actual.containsAll(expected))
+    }
+
+}
