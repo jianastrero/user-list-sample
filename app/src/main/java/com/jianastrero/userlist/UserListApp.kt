4cd56c4bdc1256b19e791c827b6bb50a3e58b51e
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-10 23:01:22 +0800

    Create Application for hilt

---
+++ app/src/main/java/com/jianastrero/userlist/UserListApp.kt
@@ -0,0 +1,13 @@
+package com.jianastrero.userlist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by jianj on 11/10/2022.
+ */
+
+@HiltAndroidApp
+class UserListApp : Application() {
+
+}
