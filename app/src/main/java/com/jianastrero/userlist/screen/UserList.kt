21fac6a5dc3014a4a9ac3a85c57b3d9b753f2c49
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-11 00:42:43 +0800

    create user list screen

---
+++ app/src/main/java/com/jianastrero/userlist/screen/UserList.kt
@@ -0,0 +1,18 @@
+package com.jianastrero.userlist.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by jianj on 11/11/2022.
+ */
+
+@Composable
+fun UserList() {
+}
+
+@Preview
+@Composable
+private fun UserListPreview() {
+    UserList()
+}
