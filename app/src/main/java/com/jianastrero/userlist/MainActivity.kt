c36d19c84fe37b71670d2ac5d0d148a5a2e6dcd7
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-10 23:01:41 +0800

    add AndroidEntryPoint annotation on MainActivity.kt

--- app/src/main/java/com/jianastrero/userlist/MainActivity.kt
+++ app/src/main/java/com/jianastrero/userlist/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jianastrero.userlist.ui.theme.UserListSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
