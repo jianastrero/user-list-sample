f30f82431e830f8aca6c1525fa69c8fd85a53af8
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-10 22:28:49 +0800

    create UserModel

---
+++ app/src/main/java/com/jianastrero/userlist/model/UserModel.kt
@@ -0,0 +1,11 @@
+package com.jianastrero.userlist.model
+
+/**
+ * Created by jianj on 11/10/2022.
+ */
+
+data class UserModel(
+    val id: String,
+    val imageUrl: String,
+    val name: String
+)
