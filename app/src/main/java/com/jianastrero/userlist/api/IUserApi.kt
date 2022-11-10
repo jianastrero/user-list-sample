700bd0ed961be7fe5ee1a59354cc2e23c844975a
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-10 23:07:07 +0800

    make getUsers() function suspended

--- app/src/main/java/com/jianastrero/userlist/api/IUserApi.kt
+++ app/src/main/java/com/jianastrero/userlist/api/IUserApi.kt
@@ -10,6 +10,6 @@ import retrofit2.http.GET
 interface IUserApi {
 
     @GET("erni-ph-mobile-team/c5b401c4fad718da9038669250baff06/raw/7e390e8aa3f7da4c35b65b493fcbfea3da55eac9/test.json")
-    fun getUsers(): List<UserModel>
+    suspend fun getUsers(): List<UserModel>
 
 }
