d2852dc0e763347312557c8a43d6fdd392b9fb76
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-11 01:24:35 +0800

    update LoadableState to enum instead of sealed class

--- app/src/main/java/com/jianastrero/userlist/enumeration/LoadableState.kt
+++ app/src/main/java/com/jianastrero/userlist/enumeration/LoadableState.kt
@@ -4,11 +4,9 @@ package com.jianastrero.userlist.enumeration
  * Created by jianj on 11/11/2022.
  */
 
-sealed class LoadableState<T : Any> {
-
-    object Initial : LoadableState<Unit>()
-    object Loading : LoadableState<Unit>()
-    class Error(val message: String) : LoadableState<Unit>()
-    class Loaded<T : Any>(val data: T) : LoadableState<T>()
-
+enum class LoadableState {
+    Initial,
+    Loading,
+    Loaded,
+    Error
 }
