c6744cf0edef01cc588937ab05ef397598a53c12
Jian James Astrero <jianjamesastrero@gmail.com>
2022-11-11 03:12:22 +0800

    implement user list screen

--- app/src/main/java/com/jianastrero/userlist/screen/UserListScreen.kt
+++ app/src/main/java/com/jianastrero/userlist/screen/UserListScreen.kt
@@ -1,18 +1,69 @@
 package com.jianastrero.userlist.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items // This import is used, sonar lint thinks its not
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jianastrero.userlist.component.ErrorScreen
+import com.jianastrero.userlist.component.UserModelItem
+import com.jianastrero.userlist.enumeration.LoadableState
+import com.jianastrero.userlist.ui.theme.UserListSampleTheme
+import com.jianastrero.userlist.viewmodel.IUserListViewModel
+import com.jianastrero.userlist.viewmodel.implementation.UserListViewModel
+import com.jianastrero.userlist.viewmodel.preview.UserListViewModelPreview
 
 /**
  * Created by jianj on 11/11/2022.
  */
 
 @Composable
-fun UserListScreen() {
+fun UserListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: IUserListViewModel = viewModel<UserListViewModel>()
+) {
+    when (viewModel.state.loadableState) {
+        LoadableState.Loading -> LoadingScreen()
+        LoadableState.Error -> ErrorScreen(
+            errorMessage = viewModel.state.errorMessage ?: "Unknown Error"
+        )
+        LoadableState.Loaded -> UserListScreenContent(modifier = modifier, viewModel = viewModel)
+        LoadableState.Initial -> Box(modifier)
+    }
+
+    viewModel.SetupLaunchedEffects()
+}
+
+@Composable
+private fun IUserListViewModel.SetupLaunchedEffects() {
+    LaunchedEffect(true) {
+        fetchUsers()
+    }
+}
+
+@Composable
+private fun UserListScreenContent(
+    modifier: Modifier,
+    viewModel: IUserListViewModel
+) {
+    LazyColumn(modifier = modifier) {
+        items(viewModel.state.users) {
+            it.UserModelItem(Modifier.fillMaxSize())
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun UserListPreview() {
-    UserListScreen()
+    UserListSampleTheme {
+        UserListScreen(
+            modifier = Modifier.fillMaxSize(),
+            viewModel = UserListViewModelPreview()
+        )
+    }
 }
